4a9c5bc54e62b45700a8af4297530ee21f6403d3
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-03-04 19:49:47 +0700

    add design main and structure app module

--- app/src/main/java/com/dicoding/githublistuser/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/githublistuser/main/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.dicoding.githublistuser.main
 
 import android.content.Intent
-import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -16,10 +17,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.title="Github users"
+        searchBar()
+
+
+    }
+
+    private fun searchBar(){
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView.editText.setOnEditorActionListener { textView, i, keyEvent ->
+                searchBar.setText(searchView.text.toString())
+                let {
+                    searchBar
+                }
+                searchView.hide()
+                false
+            }
+
+            searchView.editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    searchBar.hint = if (s.isNullOrEmpty()) getString(R.string.searchbar_hint) else s.toString()
+                }
+            })
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.Menu.main_menu, menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
