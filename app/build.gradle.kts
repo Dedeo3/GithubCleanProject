e3df29f6999b5f948062609ab58e0a7d560c6bb7
Dedeo3 <93327877+Dedeo3@users.noreply.github.com>
2024-03-13 22:13:08 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
 dependencies {
 
     implementation(project(":core"))
-    implementation project(':fav')
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
